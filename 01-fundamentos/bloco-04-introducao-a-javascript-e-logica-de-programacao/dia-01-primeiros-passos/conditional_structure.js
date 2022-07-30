8ca517dc073187217a142046f3f62313cf681b46
Sir-No-Me <antonioprofessionalmail@gmail.com>
2022-07-30 20:11:49 -0300

    Practicing if/else structure

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/conditional_structure.js
@@ -0,0 +1,10 @@
+const note = 78;
+
+if(note < 60){
+    console.log('It was not today...')
+}else if (note >= 60 && note < 80){
+    console.log('Included in waiting list');
+}else{
+    console.log('Congrats! You were approved');
+}
+
