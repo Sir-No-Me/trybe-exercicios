fe3eaecf8761fd8721e334272426057252abcbdb
Sir-No-Me <antonioprofessionalmail@gmail.com>
2022-08-05 19:06:48 -0300

    Exercicio 1 de objetos, partes 3 a 5 e mais outro teste meu de acesso aninhado

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objectsStudy.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objectsStudy.js
@@ -3,6 +3,7 @@ let player = {
     lastName: 'Silva',
     age: 34,
     medals: { golden: 2, silver: 3},
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 }
 
 let stringResult =  '';
@@ -11,12 +12,13 @@ let j=0;
 
 for(i in player){
     stringResult += ' ' + player[i];
-    if(typeof player[i] == 'object'){
+    if(typeof player[i] == 'object' && typeof player[i].isArray === false){
         for(j in player[i]){
             stringResult += ' ' + player[i][j];
         }
     }
 }
 
-
 console.log(stringResult);
+console.log('A jogadora ' + player.name + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+console.log('A jogadora possui ' + player['medals'].golden + ' medalhas de ouro ' + 'e ' + player.medals['silver'] + ' medalhas de prata');
