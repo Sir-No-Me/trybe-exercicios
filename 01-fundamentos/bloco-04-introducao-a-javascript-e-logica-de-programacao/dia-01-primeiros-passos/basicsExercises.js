3bbc586b0c3322f36aeea4702bfa0c281a9391d1
Sir-No-Me <antonioprofessionalmail@gmail.com>
2022-08-07 20:38:53 -0300

    Language fundamentals exercises, last part: 11°

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/basicsExercises.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/basicsExercises.js
@@ -118,6 +118,39 @@ function someOddNumber(n1, n2, n3){
 function profitAtThousandSales(cost, salePrice){
   if(cost < 0 || salePrice < 0)
     return 'Invalid input';
-  // let txProductCost = cost*0.2;
+  // let taxProductCost = cost*0.2;
   return salePrice-(cost+(cost*0.2));
 }
+
+function netSalary(grossSalary){
+  let tax;
+  if(grossSalary <= 1556.94){
+    tax = grossSalary*0.08;
+  }else if(grossSalary <= 2594.92){
+    tax = grossSalary*0.09;
+  }else if(grossSalary <= 5189.82){
+    tax = grossSalary*0.11;
+  }else{
+    if(grossSalary * 0.11 <= 570.88){
+      tax = grossSalary * 0.11;
+    }else{
+      tax = 570.88;
+    }
+  }
+  grossSalary -= tax;
+  console.log(tax + ' ' + grossSalary);
+  if(grossSalary <= 1903.98){
+    tax = grossSalary + 0;
+  }else if(grossSalary <= 2826.66){
+    tax = grossSalary*0.075-142.80;
+  }else if(grossSalary <= 3751.05){
+    tax = grossSalary*0.15-354.80;
+  }else if(grossSalary <= 4664.68){
+    tax = grossSalary*0.225-636.13;
+  }else{
+    tax = grossSalary*0.275-869.36;
+  }
+  return grossSalary-tax;
+}
+
+console.log(netSalary(1700));
